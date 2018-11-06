045535659630395a26605a60aad2b106171f05cd
mflores10 <itsg.mflores1@gmail.com>
2018-11-06 00:51:39 -0700

    Segunda Subida: Funcion guardarUsuario()

--- app.js
+++ app.js
@@ -7,17 +7,34 @@ firebase.initializeApp({
 // Initialize Cloud Firestore through Firebase
 var db = firebase.firestore();
 
-db.collection("users").add({
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815
-})
-.then(function(docRef) {
-    console.log("Document written with ID: ", docRef.id);
-})
-.catch(function(error) {
-    console.error("Error adding document: ", error);
-});
+
+function guardarUsuario(){
+
+	var nombre = document.getElementById('nombre').value;
+	var apellido = document.getElementById('apellido').value;
+	var fecha = document.getElementById('fecha').value;
+
+	db.collection("users").add({
+	    first: nombre,
+	    last: apellido,
+	    born: fecha
+	})
+	.then(function(docRef) {
+	    console.log("Document written with ID: ", docRef.id);
+	    limpiarUsuario();			
+
+	})
+	.catch(function(error) {
+	    console.error("Error adding document: ", error);
+	});	
+}
+
+function limpiarUsuario(){
+	document.getElementById('nombre').value = '';
+	document.getElementById('apellido').value = '';
+	document.getElementById('fecha').value = '';
+}
+
 
 
 
